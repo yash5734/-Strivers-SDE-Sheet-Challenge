fd606a165f6069032bb874fd361b131ae84e77f1
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-06-07 20:54:09 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -18,3 +18,38 @@ vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
     vector<vector<int>>ans(main.begin(),main.end());
     return ans;
 }
+
+// Optimal Approach
+
+
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+	// Write your code here.
+vector<vector<int>>ans;
+sort(arr.begin(),arr.end());
+for(int i=0;i<n;i++){
+  if(i>0 && (arr[i]==arr[i-1]))continue;
+  int j = i+1;
+  int k = n-1;
+  while(j<k){
+    int sum = arr[i]+arr[j]+arr[k];
+    
+    if(sum>K){
+      k--;
+    }
+    else if(sum<K){
+      j++;
+    }
+    else{
+      vector<int>v = {arr[i],arr[j],arr[k]};
+      ans.push_back(v);
+      j++;
+      k--;
+      while(j<k&&arr[j] == arr[j-1]) j++;
+      while(j<k && arr[k]==arr[k+1]) k--;
+    }
+  }
+}
+return ans;
+  
+}
