550f5fb43300b67c2ba42fc1095e80fd2007f4de
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-06-11 21:45:20 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,14 +1,32 @@
-int removeDuplicates(vector<int> arr, int n) {
-	// Write your code here.
-	int ind = 0;
-	int cnt = 0;
-	for(int i=0;i<n-1;i++){
-          if (arr[i] == arr[i + 1]) {
-            ind = i + 1;
-			cnt++;
-          }
+
+// optimal approch tc = O(N) SC = O(1);
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int ptr =0;
+        for (int i=0;i<n;i++){
+            if(nums[i] != nums[ptr]){
+                ptr++;
+                nums[ptr] = nums[i];
+            }
         }
-	
-	
-	return arr.size()-cnt;
-}
+        return ptr+1;
+    }
+};
+
+// brute force tc O(nlogn) sc = O(n)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> temp;
+        for(int i=0;i<nums.size();i++){
+            temp.insert(nums[i]);
+        }
+        int i=0;
+        for(auto it : temp){
+            nums[i++] = it;
+        }
+        return temp.size();
+    }
+};
