e8ebe32cb21add344105f1211b1c92ff498b7fe0
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-06-15 13:38:01 +0530

    Create 232. Implement Queue using Stacks.cpp

---
+++ 232. Implement Queue using Stacks.cpp
@@ -0,0 +1,36 @@
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    stack<int>st1;
+    stack<int>st2;
+    void push(int x) {
+        
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        st1.push(x);
+
+        while(!st2.empty()){
+            st1.push(st2.top());
+            st2.pop();
+        }
+    }
+    
+    int pop() {
+        int n = st1.top();
+        st1.pop();
+        return n;
+    }
+    
+    int peek() {
+        return st1.top();
+    }
+    
+    bool empty() {
+        return st1.empty();
+    }
+};
