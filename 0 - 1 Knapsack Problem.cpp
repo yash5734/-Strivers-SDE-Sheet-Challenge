718ffd8f8f37454e7479c59bcbde007ed86a8e78
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-07-19 11:17:55 +0530

    Create 0 - 1 Knapsack Problem.cpp

---
+++ 0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<vector<int>> dp(n,vector<int>(W+1,0));
+    
+    //Base Condition
+    
+    for(int i=wt[0]; i<=W; i++){
+        dp[0][i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + dp[ind-1][cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                
+            dp[ind][cap] = max(notTaken, taken);
+        }
+    }
+    
+    return dp[n-1][W];
+}
+
+    
+};
